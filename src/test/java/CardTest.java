0e4a891ef032e54127f4987820f8c9984f03f8d6
Абдуллин Рушан <rushan.lucky@yandex.ru>
2022-09-09 15:51:52 +0500

    Card test should add appveyor

---
+++ src/test/java/CardTest.java
@@ -0,0 +1,23 @@
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class CardTest {
+    @Test
+    void testApplication() {
+        open("http://localhost:9999");
+
+
+        $("[data-test-id = 'name'] input").setValue("Михаил");
+        $("[data-test-id  = 'phone'] input").setValue("+79278889988");
+        $("span.checkbox__box").click();
+        $("span.button__text").click();
+
+        $(withText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.")).shouldBe();
+
+    }
+}
